e775dc88900206cf396403730e8c26f6e644e405
pozmywajgary <piotr.bojas@gdansk.pl>
2018-11-29 11:24:42 +0100

    Dodaj plik

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@
 function setup() {
 
     const zoom = new gallery.Zoom();
+    new gallery.ThumbList();
+    
 
     gallery.db.photos.forEach(({thumb, image}) => {
         const photo = new gallery.Thumb(thumb);
